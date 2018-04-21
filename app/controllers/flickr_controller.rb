bb568b81c75bdf7ddec1a5f56e442a393d12cee1
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-04-21 22:27:11 +0530

    Refactoring

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -7,12 +7,14 @@ class FlickrController < ApplicationController
   def search
     @search_text = params["text"]
 
-    if @search_text.blank?
+    if @search_text.present?
+      @photos_data = flickr.photos.search(text: @search_text,
+                                          sort: 'relevance',
+                                          per_page: 10,
+                                          extras: 'url_s')
+    else
       flash[:error] = "Please enter correct search query"
       redirect_to root_path
-    else
-      @photos_data = flickr.photos.search(text: @search_text, sort: 'relevance',
-                                          per_page: 10, extras: 'url_s')
     end
   end
 
