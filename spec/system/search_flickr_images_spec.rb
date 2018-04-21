61fccfcdc2070e7dba8685f7b44326a2818fc4da
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-04-22 00:03:30 +0530

    Some minor changes

--- spec/system/search_flickr_images_spec.rb
+++ spec/system/search_flickr_images_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe "Searching flikcr images", type: :system do
   it "redirects to root page if passed text is empty" do
     visit root_path
     fill_in "text", with: ""
-    click_on("Search")
+    click_on("Submit")
     expect(current_path).to eql(root_path)
   end
 
   it "searches flickr images for given text" do
     visit root_path
     fill_in "text", with: "Coffee"
-    click_on("Search")
+    click_on("Submit")
     expect(page).to have_content("Coffee")
   end
 
